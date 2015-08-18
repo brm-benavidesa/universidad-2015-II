1fdc32416c74da296e758baa5c066a17bd862daf
Andres Benavides <andres.benavides@brm.com.co>
2015-08-18 14:45:46 -0500

    primer trabajo

---
+++ Nueva Carpeta/fecha/build/web/js/funciones.js
@@ -0,0 +1,25 @@
+var fechas = function(edad){
+  var hoy = new Date();
+  var mes = ((hoy.getMonth()+1)<= 9) ? "0" + (hoy.getMonth()+1) : (hoy.getMonth()+1);
+  var f2 =  (hoy.getFullYear()+"-"+mes+"-"+hoy.getUTCDate());
+  var f1 = edad;
+  var aFecha1 = f1.split('-');
+  var aFecha2 = f2.split('-');
+  var fFecha1 = Date.UTC(aFecha1[0], aFecha1[1] - 1, aFecha1[2]);
+  var fFecha2 = Date.UTC(aFecha2[0], aFecha2[1] - 1, aFecha2[2]);
+  var dif = fFecha2 - fFecha1;
+  var anios = Math.floor((dif / (1000 * 60 * 60 * 24 * 30.4375)))/12;
+  if(anios.toFixed()>anios){
+    var edadP = anios.toFixed()-1;
+  }else{
+     edadP = anios.toFixed();
+  }
+  if(edadP%2===0){
+    $('#cuerpo').css('background-color','#F4FA58');
+  }else{
+    $("#cuerpo").css("background-color","#0000FF");
+  }
+  //return anios;
+};
+
+
---
+++ Nueva Carpeta/fecha/web/index.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : index
+    Created on : 18/08/2015, 10:08:08 AM
+    Author     : benavidesa
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>JSP Page</title>
+  </head>
+  <body>
+    <form action="server" method="post">
+      <input type="text" name="nombre" id="nombre" />
+      <input type="date" name="fNacimiento" id="fNacimiento" />
+      <input type="submit" value="Enviar" />
+    </form>
+  </body>
+</html>
